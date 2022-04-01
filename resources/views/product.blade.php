07efcee78b33f522c37121fb6a57a0f60ae76021
BrunoBernardoHenriques <brunobernardohc@gmail.com>
2022-04-01 01:08:47 -0300

    aula7

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,2 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Produtos Brabos')
+
+@section('content')
+
 <h1> Esta é a página de Produtos</h1>
 <a href="/">Voltar home</a>
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'Brunex Eventos')
 
-
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-         <link rel="stylesheet" href="/css/styles.css">
-       
-         <script src="/js/scripts.js"></script>
-
-      
-    </head>
-    <center>
-    <body> 
+@section('content')
         <h1>Algum título</h1>
         <img src="/img/E3.jpg">
         
@@ -56,5 +43,5 @@ Número valor array{{$arr[$i]}}</p>
 @foreach($nomes as $nome)
 <p>{{$nome}}</p>
 @endforeach
-    </body>
-</html>
+ 
+@endsection
